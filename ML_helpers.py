1709cb0faa70011b05b0e718daf843544ff42a89
arieltraver <arieltraver@users.noreply.github.com>
2022-02-23 21:44:40 -0500

    gradient descent and stochastic gradient descent, untested

--- ML_helpers.py
+++ ML_helpers.py
@@ -1,5 +1,7 @@
-import numpy, math
+import numpy, math, random
 
+#arrays should take the form [[x1],y1],[[x2],y2],[[x3],y3]] 
+#this is bc our input/output is 1-d right now but maybe later will be more
 def MSE(data, theta, thetanaught=0, squared=0):
     total = 0
     for entry in data:
@@ -14,7 +16,35 @@ def MSE(data, theta, thetanaught=0, squared=0):
 arr = numpy.array([[[1],2],[[2],3],[[3],4]])
 theta = 3
 print(MSE(arr, theta, thetanaught=1, squared=1))
-print("actual answer is 9")
+
+#lil bit of functional programming for you <3
+#inputs is the x,y entries that you have
+#gradient func takes three parameters: x, y (from inputs), and theta
+def gradient_step(theta, gradient_func, inputs, step_size):
+    total = 0
+    trials = numpy.array(len(inputs))
+    index = 0
+    for entry in inputs:
+        trials[index] = gradient_func(entry[0][0], entry[1], theta)
+        index += 1
+    for trial in trials:
+        total += trial
+    total /= len(trials)
+    total *= step_size
+    return total
+
+#random stochastic gradient step
+def rand_stoch_step(theta, gradient_func, inputs, step_size):
+    #select a random piece of data
+    trial = random.randrange(len(inputs) - 1)
+    #instead of summing up all of them, just do one
+    val = gradient_func(inputs[trial][0][0], inputs[trial][1], theta)
+    val *= step_size
+    return val
+
+
+
+
 
 
 
