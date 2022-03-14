56cdcdbab42939a4d2802b3532fa09ce7dc8d8d3
arieltraver <arieltraver@users.noreply.github.com>
2022-03-14 01:18:53 -0400

    more activation functions

--- neural_nets.py
+++ neural_nets.py
@@ -1,12 +1,21 @@
-import helpers.py
+import helpers
 import math, numpy
 
 
 #takes numpy column vec, returns numpy column vec
 
+def softmax(a):
+    e_to_a = numpy.exp(a)
+    denom = e_to_a.sum(axis=0)
+    return e_to_a / denom
+
+
 def relu(z):
     return numpy.where(z>0, z[0], [0])
-print(relu(numpy.array([[-2],[3],[0]])))
 
 def drelu_dz(z):
     return numpy.where(z > 0, [1], [0])
+
+
+print(relu(numpy.array([[-2],[3],[0]])))
+print(softmax(numpy.array([[1],[1],[1]])))
